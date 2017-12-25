Corrected termination critiera signs

samplers.py
@@ -855,7 +855,7 @@ class HMC_sampler(sampler):
         return         
 
 
-    def gen_sample_NUTS(self, q_start, save_chain, verbose, first=False, first_idx_last = 5):
+    def gen_sample_NUTS(self, q_start, save_chain, verbose, first=True, first_idx_last = 5):
         """
         The same as *_static except trajectory length is determined by the termination condition
         and pathological sub-trajectories are rejected (not included in the sampling).
@@ -1034,7 +1034,7 @@ class HMC_sampler(sampler):
                                     Dq_tmp = right_q_tmp - left_q_tmp
                                     
                                     right_terminate_tmp = np.dot(Dq_tmp, right_p_tmp) < 0
-                                    left_terminate_tmp = np.dot(Dq_tmp, left_p_tmp) > 0
+                                    left_terminate_tmp = np.dot(Dq_tmp, left_p_tmp) < 0
 
                                     if left_terminate_tmp and right_terminate_tmp:
                                         # If the termination condition is satisfied by any subtree
@@ -1094,7 +1094,7 @@ class HMC_sampler(sampler):
                     # Check for termination condition
                     Dq = right_q - left_q
                     right_terminate = np.dot(Dq, right_p) < 0
-                    left_terminate = np.dot(Dq, left_p) > 0
+                    left_terminate = np.dot(Dq, left_p) < 0
 
                     # Next doubling length 2**d
                     d +=1