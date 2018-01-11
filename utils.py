cb64273ad70e562e8774a1db22a841cf13a135d5
Jae Hyeon Lee <jaekor91@gmail.com>
2018-01-11 14:19:25 -0500

    Fixed some obvious bugs.

--- utils.py
+++ utils.py
@@ -123,8 +123,8 @@ def convergence_stats(q_chain, thin_rate = 5, warm_up_num = 0):
     R = np.sqrt(var/W)
 
     #---- n_eff computation
-    n_eff = np.zeros(self.D, dtype=int) # There is an effective number for each.
-    for i in range(self.D): # For each variable
+    n_eff = np.zeros(D, dtype=int) # There is an effective number for each.
+    for i in range(D): # For each variable
         # First two base cases rho_t
         V_t1 = variogram(chains, i, 1)
         V_t2 = variogram(chains, i, 2)
@@ -132,7 +132,7 @@ def convergence_stats(q_chain, thin_rate = 5, warm_up_num = 0):
         rho_t2 = 1 - V_t2/(2*var[i])
         rho_t = [rho_t1, rho_t2]# List of autocorrelation numbers: Unknown termination number.
         t = 1 # Current t
-        while True
+        while True:
             # Compute V_t and rho_t
             V_t = variogram(chains, i, t+2)
             rho_t.append(1 - V_t/(2*var[i]))
@@ -164,7 +164,7 @@ def variogram(chains, var_num, t_lag):
     for i in range(m): # For each chain
         chain_tmp = chains[m] # Grab the chain
         # Compute the inner sum
-        inner_sum = np.sum(np.square(chain_tmp[t_lag:, var_num]-chain_tmp[:t_lag, var_num]))
+        inner_sum = np.sum(np.square(chain_tmp[t_lag:, var_num]-chain_tmp[:-t_lag, var_num]))
         # Add to the cumulative sum
         V_t += inner_sum
     V_t /= float(m*(n-t_lag))
